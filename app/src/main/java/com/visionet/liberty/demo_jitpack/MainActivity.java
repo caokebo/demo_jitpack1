d594f98476003e32e4a58f8a3d31bcee9f25b560
Liberty <caokb@visionet.com.cn>
2019-10-17 02:34:33 +0800

    init arr

--- app/src/main/java/com/visionet/liberty/demo_jitpack/MainActivity.java
+++ app/src/main/java/com/visionet/liberty/demo_jitpack/MainActivity.java
@@ -1,16 +1,28 @@
 package com.visionet.liberty.demo_jitpack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.visionet.liberty.mylibrary.utils.libraryUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView tv_test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         libraryUtil.testFunction();
+        tv_test = findViewById(R.id.tv_test);
+        tv_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,com.visionet.liberty.mylibrary.utils.TestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
